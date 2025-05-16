d400f886f04fd4fdfdd09291898b7f005b19b17d
Tony Muhm <tonymuhm@gmail.com>
2025-05-16 12:59:40 -0500

    added ignore

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, flash, render_template, redirect, request, jsonify
 from tasks import add, sync_to_phoneburner
+import uuid
+from datetime import datetime
+from data.repository import create_request_log
 
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_SECRET_KEY', "super-secret")
@@ -8,8 +11,28 @@ api_key = os.getenv(
     'API_KEY', "wvMxNEJXK0GQ4Jnz7QhR2KUyg2hyxJAQwtqMuiHGnrCVG2Hs8H5pvHFSEz0Qeq7S")
 
 
+request_id = None
+request_time = None
+request_type = None
+request_data = None
+request_status = None
+response_time = None
+response_status = None
+
 @app.before_request
 def before_request():
+    global request_id, request_time, request_type, request_data, request_status, response_time, response_status
+    request_id = str(uuid.uuid4())
+    request_time = datetime.now()
+    request_type = request.path
+    request_data = request.args
+    request_status = 'success'
+    response_time = None
+    response_status = None
+    response_data = None
+    create_request_log(request_id, request_type,
+                       request_data, request_status, request_time)
+
     if request.path == '/':
         return
     api_key_param = request.args.get('apikey')
@@ -25,10 +48,11 @@ def main():
 
 @app.route('/api/v1/phoneburner/sync', methods=['POST'])
 def api_sync_to_phoneburner():
-    pd_ref = request.json.get('pd_ref')
-    sync_to_phoneburner.delay(pd_ref)
-    if not pd_ref:
-        return jsonify({'error': 'Missing pd_ref'}), 400
+    print(request.json)
+    #pd_ref = request.json.get('pd_ref')
+    # sync_to_phoneburner.delay(pd_ref)
+    # if not pd_ref:
+    #     return jsonify({'error': 'Missing pd_ref'}), 400
     return jsonify({'message': 'Syncing to Phoneburner'}), 200
 
 
